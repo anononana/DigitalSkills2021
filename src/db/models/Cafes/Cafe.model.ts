cf43783574abfbdf2eb2fc07766e9d574337cbbf
anononana <anna.vlasova.01@gmail.com>
2021-09-23 14:52:38 +0300

    Cafe

---
+++ src/db/models/Cafes/Cafe.model.ts
@@ -0,0 +1,17 @@
+import { Table, Column, Model, DataType, Unique, AllowNull, Scopes,
+    HasMany, ForeignKey, BelongsTo, IsEmail, Default, DefaultScope } from 'sequelize-typescript';
+import User from '../User/User.model';
+@Table({
+  paranoid: true,
+  timestamps: true, 
+})
+  class Cafe extends Model<Cafe> {
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    cashToken!: string;
+
+    @Column(DataType.INTEGER)
+    cashValue!: number;
+  }
+  
+  export default Cafe;
